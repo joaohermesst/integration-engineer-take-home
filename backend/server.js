c65293ea987d61b2309ebce67b314d6a093abcbf
João Henrique <joaohenriquehs2002@gmail.com>
2024-07-12 13:29:10 -0300

    feat: Separate backend into a new monorepo workspace

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -9,16 +9,13 @@ app.use(bodyParser.json());
 let tasks = [];
 let nextTaskId = 1;
 
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-app.post('/tasks', (req, res) => {
-});
+app.post("/tasks", (req, res) => {});
 
-
-app.delete('/tasks/:id', (req, res) => {
-});
+app.delete("/tasks/:id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
