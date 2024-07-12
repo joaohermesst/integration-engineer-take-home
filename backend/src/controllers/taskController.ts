2ef1603f7fb6c5e822d674aaef1ecf24a0fc3d87
João Henrique <joaohenriquehs2002@gmail.com>
2024-07-12 15:02:50 -0300

    feat: Add create and delete task handling

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -4,11 +4,29 @@ import Task from "../interfaces/Task";
 let tasks: Task[] = [];
 let nextTaskId = 1;
 
-const getTasks = async (request: Request, response: Response) => {
+const getTasks = async (_request: Request, response: Response) => {
   return response.json(tasks);
 };
 
-const createTask = async (request: Request, response: Response) => {};
-const deleteTask = async (request: Request, response: Response) => {};
+const createTask = async (request: Request, response: Response) => {
+  const { title, description } = request.body;
+  const newTask: Task = {
+    id: nextTaskId++,
+    title,
+    description,
+  };
+  tasks.push(newTask);
+  return response.status(201).json(newTask);
+};
+
+const deleteTask = async (request: Request, response: Response) => {
+  const id = parseInt(request.params.id);
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+  if (taskIndex === -1) {
+    return response.status(404).json({ error: "Task not found" });
+  }
+  tasks.splice(taskIndex, 1);
+  return response.status(204).send();
+};
 
 export { getTasks, createTask, deleteTask };
