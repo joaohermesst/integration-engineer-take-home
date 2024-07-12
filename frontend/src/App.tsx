c8cd68a4dd305f1b0aa970cfaad664a882aeb718
João Henrique <joaohenriquehs2002@gmail.com>
2024-07-12 15:27:39 -0300

    fix: Add react dependency to solve frontend build problem

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,35 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 type Task = {
-  id: number,
-  title: string,
-  description: string
-}
+  id: number;
+  title: string;
+  description: string;
+};
 
-function App() {
+const App: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [formData, setFormData] = useState({ title: '', description: '' });
+  const [formData, setFormData] = useState({ title: "", description: "" });
 
   useEffect(() => {
     fetchTasks();
   }, []);
 
   const fetchTasks = async () => {
-    const response = await fetch('http://localhost:8000/tasks')
+    const response = await fetch("http://localhost:8000/tasks");
     const tasks = await response.json();
     setTasks(tasks);
   };
 
   /* Complete the following functions to hit endpoints on your server */
-  const createTask = async () => {
-  };
+  const createTask = async () => {};
 
-  const deleteTask = async (id: string) => {
-  };
-
+  const deleteTask = async (id: string) => {};
 
   return (
     <div>
       <h1>Task Management App</h1>
       <ul>
-        {tasks.map(task => (
+        {tasks.map((task) => (
           <li key={task.id}>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
@@ -46,18 +43,20 @@ function App() {
           type="text"
           placeholder="Title"
           value={formData.title}
-          onChange={e => setFormData({ ...formData, title: e.target.value })}
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
         />
         <input
           type="text"
           placeholder="Description"
           value={formData.description}
-          onChange={e => setFormData({ ...formData, description: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
         />
         <button onClick={createTask}>Create</button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
