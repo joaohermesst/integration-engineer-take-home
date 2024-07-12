79e86e47f1ca2dcb00099edc043f136fa308bef5
João Henrique <joaohenriquehs2002@gmail.com>
2024-07-12 15:42:44 -0300

    feat: Add CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import taskRoutes from "./routes/taskRoutes";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/tasks", taskRoutes);
 
