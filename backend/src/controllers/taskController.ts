da13e72f943e2acda8b1b054b9d3ee4eeffc65df
João Henrique <joaohenriquehs2002@gmail.com>
2024-07-12 14:47:08 -0300

    type: Add Task interface to correctly type the entity

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
+import Task from "../interfaces/Task";
 
-let tasks: number[] = [];
+let tasks: Task[] = [];
 let nextTaskId = 1;
 
 const getTasks = async (request: Request, response: Response) => {
